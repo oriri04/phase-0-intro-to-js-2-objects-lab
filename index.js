f55f6c6295ae8108abd4e32ae0f7102115954f8a
oriri04 <152679239+oriri04@users.noreply.github.com>
2023-12-21 20:38:46 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution in this file!
+const employee = {
+    label: "Esther",
+    Residence: "Nairobi"
+};
+console.log(employee);
+
+function updateEmployeeWithKeyAndValue(object,key,value) {
+    const newEmployee = {...employee};
+    newEmployee[key] = value;
+    return newEmployee;
+}
+console.log(updateEmployeeWithKeyAndValue(employee,"label","Jennifer"))
+
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object,key,value) {
+object[key] = value;
+    return object;
+}
+destructivelyUpdateEmployeeWithKeyAndValue(employee,"label","Jane")
+console.log(employee);
+
+function deleteFromEmployeeByKey(object, key) {
+    const deletedLabel = {...object}
+    delete deletedLabel[key];
+    return deletedLabel
+}
+console.log(deleteFromEmployeeByKey(employee,"label",)); 
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
+destructivelyDeleteFromEmployeeByKey(object,label)
